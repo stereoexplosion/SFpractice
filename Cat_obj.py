7a934b29775f6bc6802237f0206964afe5b46b45
stereoexplosion <71786407+stereoexplosion@users.noreply.github.com>
2020-11-08 16:00:47 +0300

    Create Cat_obj.py

---
+++ Cat_obj.py
@@ -0,0 +1,5 @@
+from CLass_cat import Cat
+cat_1 = Cat("Барон", "Мальчик", 2)
+cat_2 = Cat("Сэм", "Мальчик", 2)
+print(cat_1.name, ",", "Пол:", cat_1.gender, ",", "Возраст:", cat_1.age)
+print(cat_2.name, ",", "Пол:", cat_2.gender, ",", "Возраст:", cat_2.age)
